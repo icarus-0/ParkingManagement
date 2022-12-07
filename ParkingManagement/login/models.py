0dc1622401b58916f0a93c2b30fec2b6f764bd0d
icarus-0 <naman.chandel_cs17@gla.ac.in>
2022-12-07 15:05:52 +0530

    parking agent models created

--- ParkingManagement/login/models.py
+++ ParkingManagement/login/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class BookingCounterAgent(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    def __str__(self):
+         return str(self.id)+'_'+self.name
+
+class ParkingZoneAssitant(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    def __str__(self):
+         return str(self.id)+'_'+self.name
