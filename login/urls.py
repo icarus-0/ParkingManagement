fae34e5ff0c582ff0728116cf268c9fe94e68c92
icarus-0 <46519285+icarus-0@users.noreply.github.com>
2022-12-07 14:31:32 +0530

    Delete login directory

--- login/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('signup',views.SignUp.as_view(),name='signup'),
-    path('signin',views.SignIn.as_view(),name='signin'),
-    path('logout',views.logout,name='logout'),
-]
